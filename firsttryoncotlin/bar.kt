aea919da37b5d14db21b264caea581cdde5f31b4
LLI <lastlordigor@gmail.com>
2023-03-19 18:39:55 +0400

    Code Tri1

---
+++ firsttryoncotlin/bar.kt
@@ -0,0 +1,1 @@
+class Bar (val Email: String)
---
+++ firsttryoncotlin/car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private  final String email;
+
+    public Car(String email){
+        this.email=email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
